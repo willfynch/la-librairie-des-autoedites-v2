0e144c9b77d0dbca1cfad0acfe740091e14b1f8a
willfynch <william.finzy@protonmail.com>
2025-09-02 20:34:52 +0200

    poc / test data fetching at build time

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('la-librairie-des-autoedites-v2');
+  protected readonly http = inject(HttpClient);
+
+  posts: any[] = [];
+
+  ngOnInit() {
+    // This data will be fetched at build time
+    this.http.get<any[]>('https://jsonplaceholder.typicode.com/posts').subscribe((data) => {
+      this.posts = data.slice(0, 5); // Get first 5 posts
+    });
+  }
 }
